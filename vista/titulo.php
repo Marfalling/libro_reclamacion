f4ce7ce3124cf18dd377e83cec43367ec3c9a4a0
Marfalling <dame.tufuerza.pegazo988@gmail.com>
2024-11-07 12:20:36 -0500

    css

--- vista/titulo.php
+++ vista/titulo.php
@@ -1,5 +1,20 @@
-    <div class="card text-center mb-4">
-        <div class="card-header">
-            LIBRO DE RECLAMACIONES
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Libro de Reclamaciones</title>
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Black+Han+Sans&display=swap">
+</head>
+<body>
+    <div class="container ">
+        <div class="card shadow-lg card-reclamaciones text-center mb-4">
+            <div class="card-header  ">
+                LIBRO DE RECLAMACIONES
+            </div>
         </div>
     </div>
+</body>
+</html>
