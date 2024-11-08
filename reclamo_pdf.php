9ffc930ddf0f60385f3bbc83998a771b8df75738
Marfalling <dame.tufuerza.pegazo988@gmail.com>
2024-11-08 18:53:22 -0500

    cambios de utf8 decode por estar deprecated y cambio de color en celdas

--- reclamo_pdf.php
+++ reclamo_pdf.php
@@ -91,7 +91,7 @@ class PDF extends FPDF
         $this->SetFont('Arial', 'B', 12);
         
         // Celda para el título principal
-        $this->Cell(0, 10, utf8_decode('Anexo I: Formato de Hoja de Reclamaciones del Libro de Reclamaciones'), 0, 1, 'C');
+        $this->Cell(0, 10, mb_convert_encoding('Anexo I: Formato de Hoja de Reclamaciones del Libro de Reclamaciones', 'ISO-8859-1', 'UTF-8'), 0, 1, 'C');
         $this->Ln(5);    
     }
 
@@ -107,20 +107,20 @@ class PDF extends FPDF
 
         // Configuración del estilo
         $this->SetFont('Arial', 'B', 10);
-
+        $this->SetFillColor(220, 220, 220);
         // Primera fila: Títulos
-        $this->Cell(120, 8, 'LIBRO DE RECLAMACIONES', 1, 0, 'C'); // Título
+        $this->Cell(120, 8, 'LIBRO DE RECLAMACIONES', 1, 0, 'C', true); // Título
         $this->Cell(70, 8, 'HOJA DE RECLAMACION', 1, 1, 'C'); // Cuadro a la derecha
 
         // Segunda fila: Fecha
         $this->Cell(25, 10, 'FECHA:', 1, 0, 'C');                  // Etiqueta de Fecha
-        $this->Cell(35, 10, utf8_decode ('').utf8_decode($dia), 1, 0, 'C');                     // Celda para el día
-        $this->Cell(25, 10, ''.utf8_decode($mes), 1, 0, 'C');                     // Celda para el mes
-        $this->Cell(35, 10,  utf8_decode ('').utf8_decode($ano), 1, 0, 'C');                     // Celda para el año
+        $this->Cell(35, 10, '' . mb_convert_encoding($dia, 'ISO-8859-1', 'UTF-8'), 1, 0, 'C');
+        $this->Cell(25, 10, ''.mb_convert_encoding($mes, 'ISO-8859-1', 'UTF-8'), 1, 0, 'C');                     // Celda para el mes
+        $this->Cell(35, 10, ''.mb_convert_encoding ($ano, 'ISO-8859-1', 'UTF-8'), 1, 0, 'C');                     // Celda para el año
 
         
         // Alinear la celda de N° a la misma altura que HOJA DE RECLAMACION
-        $this->Cell(70, 10, utf8_decode ('N° ').utf8_decode($id_reclamo), 1, 1, 'C');   // Cuadro para el número de reclamación
+        $this->Cell(70, 10, mb_convert_encoding ('N° ', 'ISO-8859-1', 'UTF-8').mb_convert_encoding($id_reclamo, 'ISO-8859-1', 'UTF-8'), 1, 1, 'C');   // Cuadro para el número de reclamación
         
 
         // Datos del proveedor
@@ -128,48 +128,51 @@ class PDF extends FPDF
         $this->MultiCell(0, 8, "[NOMBRE DE LA PERSONA NATURAL O RAZON SOCIAL DE LA PERSONA JURIDICA / RUC DEL PROVEEDOR]\n[DOMICILIO DEL ESTABLECIMIENTO DONDE SE COLOCA EL LIBRO DE RECLAMACIONES/ CODIGO DE IDENTIFICACION]", 1, 'C');
         
         $this->SetFont('Arial', 'B', 8); // Restablece el tamaño de la fuente a 12, si es necesario para el siguiente contenido
-        
-        $this->Cell(0, 8, '1. IDENTIFICACION DEL CONSUMIDOR RECLAMANTE', 1, 1, 'L');
-        $this->Cell(0, 8, 'NOMBRE: '. utf8_decode($nom) . ' ' .utf8_decode($ape_paterno) . ' ' .utf8_decode($ape_materno),1, 0); // Asigna un ancho específico
+        $this->SetFillColor(220, 220, 220);
+
+        $this->Cell(0, 8, '1. IDENTIFICACION DEL CONSUMIDOR RECLAMANTE', 1, 1, 'L',true);
+        $this->Cell(0, 8, 'NOMBRE: '. mb_convert_encoding($nom, 'ISO-8859-1', 'UTF-8') . ' ' .mb_convert_encoding($ape_paterno, 'ISO-8859-1', 'UTF-8') . ' ' .mb_convert_encoding($ape_materno, 'ISO-8859-1', 'UTF-8'),1, 0); // Asigna un ancho específico
         $this->Cell(0, 8, '', 0, 0); // Celda vacía para el nombre
         $this->Ln(); // Salta a la siguiente línea
-        $this->Cell(0, 8, 'DOMICILIO: '. utf8_decode($dir), 1, 0); // Asigna un ancho específico
+        $this->Cell(0, 8, 'DOMICILIO: '. mb_convert_encoding($dir, 'ISO-8859-1', 'UTF-8'), 1, 0); // Asigna un ancho específico
         $this->Cell(0, 8, '', 0, 0); // Celda vacía para el domicilio
         $this->Ln(); // Salta a la siguiente línea
-        $this->Cell(75, 10, 'DNI: / CE: ' . utf8_decode($tipo_documento) . ' : ' . utf8_decode($n_documento), 1, 0, 'L');
-        $this->Cell(0, 10, utf8_decode ('TÉLEFONO / EMAIL: '). utf8_decode($cel) . ' / ' . utf8_decode($email), 1, 0, 'L');                  // Etiqueta de TELEFONO Y EMAIL
+        $this->Cell(75, 10, 'DNI: / CE: ' .  mb_convert_encoding($tipo_documento, 'ISO-8859-1', 'UTF-8') . ' : ' . mb_convert_encoding($n_documento, 'ISO-8859-1', 'UTF-8'), 1, 0, 'L');
+        $this->Cell(0, 10, mb_convert_encoding ('TÉLEFONO / EMAIL: ', 'ISO-8859-1', 'UTF-8'). mb_convert_encoding($cel, 'ISO-8859-1', 'UTF-8') . ' / ' . mb_convert_encoding($email, 'ISO-8859-1', 'UTF-8'), 1, 0, 'L');                  // Etiqueta de TELEFONO Y EMAIL
         $this->Ln(); // Salta a la siguiente línea
-        $this->Cell(0, 8, 'PADRE O MADRE: [PARA EL CASO DE MENORES DE EDAD]'. utf8_decode($menor_edad), 1, 1);
+        $this->Cell(0, 8, 'PADRE O MADRE: [PARA EL CASO DE MENORES DE EDAD]'. mb_convert_encoding($menor_edad, 'ISO-8859-1', 'UTF-8'), 1, 1);
         
 
         $this->SetFont('Arial', 'B', 8); // Restablece el tamaño de la fuente a 12, si es necesario para el siguiente contenido
         //IDENTIFICACION DEL BIEN CONTRATADO
-        $this->Cell(0, 8, '2. IDENTIFICACION DEL BIEN CONTRATADO', 1, 1, 'L');
+        $this->SetFillColor(220, 220, 220);
+        $this->Cell(0, 8, '2. IDENTIFICACION DEL BIEN CONTRATADO', 1, 1, 'L',true);
         $this->Cell(30, 8, 'PRODUCTO', 1, 0);
         $this->Cell(15, 8, ($tipo_bien == 'Producto' ? 'X' : ''), 1, 0, 'C'); // Marca "X" si es Producto
-        $this->Cell(0, 8, 'MONTO RECLAMADO: '. utf8_decode($monto), 1, 0);
+        $this->Cell(0, 8, 'MONTO RECLAMADO: '. mb_convert_encoding($monto, 'ISO-8859-1', 'UTF-8'), 1, 0);
         $this->Ln(); // Salta a la siguiente línea
 
         // Usar MultiCell para "DESCRIPCIÓN"
         $this->Cell(30, 8, 'SERVICIO', 1, 0);
         $this->Cell(15, 8, ($tipo_bien == 'Servicio' ? 'X' : ''), 1, 0, 'C'); // Marca "X" si es Servicio
-        $this->Cell(0, 8, utf8_decode ('DESCRIPCIÓN: '). utf8_decode($descripcion), 1, 0); // Crea una celda más alta para "DESCRIPCIÓN"
+        $this->Cell(0, 8, mb_convert_encoding ('DESCRIPCIÓN: ', 'ISO-8859-1', 'UTF-8'). mb_convert_encoding($descripcion, 'ISO-8859-1', 'UTF-8'), 1, 0); // Crea una celda más alta para "DESCRIPCIÓN"
         $this->Ln(); // Salta a la siguiente línea
 
         
         $this->SetFont('Arial', 'B', 8); // Restablece el tamaño de la fuente
+        $this->SetFillColor(220, 220, 220);
         // DETALLE DE LA RECLAMACION
-        $this->Cell(100, 8, '3. DETALLE DE LA RECLAMACION Y EL PEDIDO DEL CONSUMIDOR', 1, 0, 'L'); // Título
-        $this->Cell(30, 8, utf8_decode('RECLAMO:'), 1, 0, 'C'); // Crea una celda para "RECLAMO" al lado del título
+        $this->Cell(100, 8, '3. DETALLE DE LA RECLAMACION Y EL PEDIDO DEL CONSUMIDOR', 1, 0, 'L', true); // Título
+        $this->Cell(30, 8, mb_convert_encoding('RECLAMO:', 'ISO-8859-1', 'UTF-8'), 1, 0, 'C'); // Crea una celda para "RECLAMO" al lado del título
         $this->Cell(15, 8, ($tipo_reclamo == 'Reclamo' ? 'X' : ''), 1, 0, 'C'); // Espacio vacío para "RECLAMO"
-        $this->Cell(30, 8, utf8_decode('QUEJA:'), 1, 0, 'C'); // Crea una celda para "QUEJA:" al lado de "RECLAMO"
+        $this->Cell(30, 8, mb_convert_encoding('QUEJA:', 'ISO-8859-1', 'UTF-8'), 1, 0, 'C'); // Crea una celda para "QUEJA:" al lado de "RECLAMO"
         $this->Cell(15, 8, ($tipo_reclamo == 'Queja' ? 'X' : ''), 1, 1, 'C'); // Espacio vacío para "QUEJA"
         
         // Celda ancha para "DETALLE:"
-        $this->Cell(0, 30, 'DETALLE: '. utf8_decode($detalle_reclamo), 1, 1, 'L'); // Celda ancha para "DETALLE:"
+        $this->Cell(0, 30, 'DETALLE: '. mb_convert_encoding($detalle_reclamo, 'ISO-8859-1', 'UTF-8'), 1, 1, 'L'); // Celda ancha para "DETALLE:"
         
        // Fila para la firma del proveedor y cuadro vacío
-       $this->Cell(125, 30, 'PEDIDO: '. utf8_decode($pedido), 1, 0, 'L'); // Cuadro vacío a la izquierda
+       $this->Cell(125, 30, 'PEDIDO: '. mb_convert_encoding($pedido, 'ISO-8859-1', 'UTF-8'), 1, 0, 'L'); // Cuadro vacío a la izquierda
        // Coloca el texto "FIRMA DEL CONSUMIDOR" centrado
        $this->Cell(0, 10, 'FIRMA DEL CONSUMIDOR', 1, 0, 'C'); 
 
@@ -179,17 +182,19 @@ class PDF extends FPDF
 
         // Restablece el tamaño de la fuente
         $this->SetFont('Arial', 'B', 8);
+        $this->SetFillColor(220, 220, 220);
 
         // TÍTULO: 4. OBSERVACIONES Y ACCIONES ADOPTADAS POR EL PROVEEDOR
-        $this->Cell(0, 8, '4. OBSERVACIONES Y ACCIONES ADOPTADAS POR EL PROVEEDOR', 1, 0, 'L'); // Título
+        $this->Cell(0, 8, '4. OBSERVACIONES Y ACCIONES ADOPTADAS POR EL PROVEEDOR', 1, 0, 'L', true); // Título
         $this->Ln();
 
+        
         // Segunda fila: Fecha
         $this->Cell(80, 10, 'FECHA DE COMUNICACION DE LA RESPUESTA:', 1, 0, 'C'); // Etiqueta de Fecha
-        $this->Cell(15, 10, utf8_decode('DÍA'), 1, 0, 'C'); // Celda para el día
+        $this->Cell(15, 10, mb_convert_encoding('DÍA', 'ISO-8859-1', 'UTF-8'), 1, 0, 'C'); // Celda para el día
         $this->Cell(15, 10, 'MES', 1, 0, 'C'); // Celda para el mes
-        $this->Cell(15, 10, utf8_decode('AÑO'), 1, 0, 'C');
-        $this->Cell(0, 10, utf8_decode('FIRMA DEL PROVEEDOR'), 1, 0, 'C');
+        $this->Cell(15, 10, mb_convert_encoding('AÑO', 'ISO-8859-1', 'UTF-8'), 1, 0, 'C');
+        $this->Cell(0, 10, mb_convert_encoding('FIRMA DEL PROVEEDOR', 'ISO-8859-1', 'UTF-8'), 1, 0, 'C');
 
         $this->Ln(); // Salto de línea después de la fila de fecha
 
@@ -198,7 +203,7 @@ class PDF extends FPDF
         $this->Cell(0, 20, '', 1, 1, 'C'); // Celda de FIRMA 
 
         $this->Cell(100,12, 'Reclamo: Disconformidad relacionada a los productos o servicios.', 1, 0, 'L');        
-        $this->MultiCell(0,4, utf8_decode("Queja: Disconformidad no relacionada a los productos o servicios; o, \nmalestar o descontento respecto a la atención al publico."), 1, 'L');
+        $this->MultiCell(0,4, mb_convert_encoding("Queja: Disconformidad no relacionada a los productos o servicios; o, \nmalestar o descontento respecto a la atención al publico.", 'ISO-8859-1', 'UTF-8'), 1, 'L');
         $this->Ln();
         
 
@@ -208,8 +213,8 @@ class PDF extends FPDF
     {
 
         $this->SetFont('Arial', 'I', 8);
-        $this->MultiCell(0, 5,utf8_decode( '* La formulación del reclamo no impide acudir a otras vías de solución de controversias ni es requisito previo para interponer una denuncia ante el INDECOPI.' . "\n" .
-                        '* El proveedor debe dar respuesta al reclamo o queja en un plazo no mayor a quince (15) días hábiles, el cual es improrrogable.'), 0, 'L');
+        $this->MultiCell(0, 5,mb_convert_encoding( '* La formulación del reclamo no impide acudir a otras vías de solución de controversias ni es requisito previo para interponer una denuncia ante el INDECOPI.' . "\n" .
+                        '* El proveedor debe dar respuesta al reclamo o queja en un plazo no mayor a quince (15) días hábiles, el cual es improrrogable.', 'ISO-8859-1', 'UTF-8'), 0, 'L');
         // Establecer la posición en Y a la altura total de la página menos el margen inferior
         $this->SetY(-($this->bMargin + 10)); // 15 es la distancia desde el borde inferior que deseas para la imagen
         
